f54d5c475744df43a4efb84d882441132ebae877
Niyi Ogungbemi <niyiment@gmail.com>
2025-03-25 00:37:49 +0100

    Project setup

---
+++ src/main/java/com/niyiment/samples/data_cleaning/DataCleaningApplication.java
@@ -0,0 +1,13 @@
+package com.niyiment.samples.data_cleaning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCleaningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCleaningApplication.class, args);
+	}
+
+}
